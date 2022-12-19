98d7402f832a7949b269caee2ae0d1a420a200af
blaukc <brandonlau1@hotmail.com>
2022-12-19 10:22:35 +0800

    Add database sql file

---
+++ database.sql
@@ -0,0 +1,37 @@
+-- 1
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE ForumUsers (
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	name VARCHAR ( 50 ) UNIQUE NOT NULL,
+	date_created TIMESTAMP NOT NULL
+);
+
+CREATE TABLE Categories (
+	id serial PRIMARY KEY,
+	category VARCHAR ( 50 ) UNIQUE NOT NULL
+);
+
+CREATE TABLE Posts (
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	poster uuid NOT NULL,
+	category serial NOT NULL,
+	date_created TIMESTAMP NOT NULL,
+	date_updated TIMESTAMP NOT NULL,
+	CONSTRAINT fk_poster FOREIGN KEY (poster) REFERENCES ForumUsers (id) ON DELETE SET NULL,
+	CONSTRAINT fk_category FOREIGN KEY (category) REFERENCES Categories (id) ON DELETE SET NULL,
+	title TEXT NOT NULL
+);
+
+CREATE TABLE Comments (
+	id serial PRIMARY KEY,
+	commenter uuid NOT NULL,
+	post uuid NOT NULL,
+	comment TEXT NOT NULL,
+	date_created TIMESTAMP NOT NULL,
+	CONSTRAINT fk_commenter FOREIGN KEY (commenter) REFERENCES ForumUsers (id) ON DELETE SET NULL,
+	CONSTRAINT fk_post FOREIGN KEY (post) REFERENCES Posts (id) ON DELETE CASCADE
+);
+
+--
