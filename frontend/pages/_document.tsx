45e75039af6acb935f33a9ac16426ed1ebf529b0
blaukc <brandonlau1@hotmail.com>
2023-01-24 18:09:55 +0800

    Add insecure header

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,13 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body style={{ margin: 0 }}>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <meta
+                    http-equiv="Content-Security-Policy"
+                    content="upgrade-insecure-requests"
+                />
+            </Head>
+            <body style={{ margin: 0 }}>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
